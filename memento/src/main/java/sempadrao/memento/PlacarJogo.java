52966fb519c94e87221f7ac602c0d7d2b415876d
Italo <italobm2011@gmail.com>
2026-05-18 21:03:33 -0300

    Trabalho de Arquitetura e Projeto de Software: Padrões de Projeto

--- memento/src/main/java/sempadrao/memento/PlacarJogo.java
+++ memento/src/main/java/sempadrao/memento/PlacarJogo.java
@@ -1,4 +1,21 @@
 package sempadrao.memento;
 
 public class PlacarJogo {
+    // O PROBLEMA: Para o cliente conseguir salvar o estado, as variáveis precisam
+    // estar expostas (ter getters e setters para tudo), quebrando o encapsulamento.
+    private int golsTimeCasa = 0;
+    private int golsTimeFora = 0;
+
+    public void marcarGolCasa() { golsTimeCasa++; }
+    public void marcarGolFora() { golsTimeFora++; }
+
+    public int getGolsTimeCasa() { return golsTimeCasa; }
+    public void setGolsTimeCasa(int golsTimeCasa) { this.golsTimeCasa = golsTimeCasa; }
+
+    public int getGolsTimeFora() { return golsTimeFora; }
+    public void setGolsTimeFora(int golsTimeFora) { this.golsTimeFora = golsTimeFora; }
+
+    public String getPlacarFormatado() {
+        return "Casa " + golsTimeCasa + " x " + golsTimeFora + " Fora";
+    }
 }
--- memento/src/test/java/sempadrao/memento/CabineVARTest.java
+++ memento/src/test/java/sempadrao/memento/CabineVARTest.java
@@ -1,4 +1,26 @@
 package sempadrao.memento;
 
-public class CabineVARTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CabineVARTest {
+
+    @Test
+    void deveAnularGolManualmente() {
+        PlacarJogo placar = new PlacarJogo();
+
+        // O PROBLEMA: O cliente (VAR) tem que gerenciar as variáveis de estado manualmente
+        int backupGolsCasa = placar.getGolsTimeCasa();
+        int backupGolsFora = placar.getGolsTimeFora();
+
+        // Gol!
+        placar.marcarGolCasa();
+        assertEquals("Casa 1 x 0 Fora", placar.getPlacarFormatado());
+
+        // Anulou! O cliente tem que "injetar" os valores de volta manualmente
+        placar.setGolsTimeCasa(backupGolsCasa);
+        placar.setGolsTimeFora(backupGolsFora);
+
+        assertEquals("Casa 0 x 0 Fora", placar.getPlacarFormatado());
+    }
 }
